5480ad33f9c0755f3d453ed928011e82312cb280
arjun <arjun@realityclick.io>
2019-01-19 23:48:41 +0200

    Introducing CustomError & checking values are numbers

--- index.js
+++ index.js
@@ -1,34 +1,17 @@
+
 let devices = [[0,0],[100,100],[15,10],[18,18]];
-let stations = [[0, 0, 10],[20, 20, 5],[10, 0, 12]];
+let stations = [[0, 0, 10],[20, 20, -5],[10, 0, 12]];
 
-// iterate over each given device
-for (let [x,y] of devices) {
- // placeholder variables for storing power and coords of best station
- let pmax = 0;
- let bestStationCoords = null; 
- 
- // iterate over each station to calculate power
- for (let [sx,sy,sr] of stations) {
-  let stationPower = powerBetweenDeviceAndStation([x,y], [sx, sy, sr]);
-
-  // store only station with highest power
-  if (stationPower > pmax) {
-    pmax = stationPower;
-    bestStationCoords = [sx, sy];
-  }
- }
-
- if (pmax == 0) {
-   console.log(`No link station within reach for point ${x},${y}`);
- } else {
-  console.log(`Best link station for point ${x},${y} is ${bestStationCoords} with power ${pmax}`);
- }
-}
+class CustomError extends Error {};
 
 // power based on the given function description
 function powerBetweenDeviceAndStation([px,py],[sx,sy,sr]){
+  if(isNaN(px) || isNaN(px) || isNaN(sx) || isNaN(sy) || isNaN(sr)){
+    throw new CustomError(`One/more invalid values: ${px}, ${py}, ${sx}, ${sy}, ${sr}`);
+  }
+
   if (sr < 0) {
-    throw new Error("Reach cannot be negative");
+    throw new CustomError(`Reach is negative in station: ${sx}, ${sy}, ${sr}`);
   }
 
   let dist = distanceBetween([px,py],[sx,sy]);
@@ -46,6 +29,42 @@ function distanceBetween([x1, y1],[x2, y2]){
 function powerFromDistance(distance, reach){
   return Math.pow((reach - distance), 2);
 } 
+
+// iterate over each given device
+for (let [x,y] of devices) {
+  // placeholder variables for storing power and coords of best station
+  let pmax = 0;
+  let bestStationCoords = null; 
+  
+ // iterate over each station to calculate power
+ for (let [sx,sy,sr] of stations) {
+   let stationPower;
+  
+   try {
+     stationPower = powerBetweenDeviceAndStation([x,y], [sx, sy, sr]);
+    } catch(e){
+      if (e instanceof CustomError) {
+        console.log(`SOMETHING WENT WRONG: ${e}`);
+      } else {
+        throw e;
+      }
+    }
+    
+    // store only station with highest power
+    if (stationPower > pmax) {
+      pmax = stationPower;
+      bestStationCoords = [sx, sy];
+    }
+  }
+  
+  if (pmax == 0) {
+    console.log(`No link station within reach for point ${x},${y}`);
+  } else {
+    console.log(`Best link station for point ${x},${y} is ${bestStationCoords} with power ${pmax}`);
+  }
+}
+
+
 
 
 
@@ -68,12 +87,13 @@ function testPowerBetweenDeviceAndStation(){
     [[0,0], [0,0, 10], 100],
     [[3,4], [0,0,10], 25],
     [[3,4], [0,0,4], 0],
-    [[-8,6], [0,0,11], 1]
+    [[-8,6], [0,0,-11], 1],
+    [[-8,6], [0,0,"11d"], 1],
   ];
 
   testVars.forEach(([[px,py], [sx, sy,sr], power]) => {
     if(powerBetweenDeviceAndStation([px,py], [sx,sy, sr]) != power){
-      throw new Error(`Error in calculation for ${px}, ${py}, ${sx}, ${sy}, ${sr}, ${power}`)
+      throw new CustomError(`Error in calculation for ${px}, ${py}, ${sx}, ${sy}, ${sr}, ${power}`)
     }  
   });
 }
@@ -89,24 +109,8 @@ function testDistanceBetween(){
 
   testVars.forEach(([[x1,y1], [x2,y2], distance]) => {
     if(distanceBetween([x1,y1], [x2,y2]) != distance){
-      throw new Error(`Error in calculation for ${x1}, ${y1}, ${x2}, ${y2}, ${distance}`);
+      throw new CustomError(`Error in calculation for ${x1}, ${y1}, ${x2}, ${y2}, ${distance}`);
     }
   })
-
-  // if(distanceBetween([0,0], [0,0]) != 0){
-  //   console.log("error")
-  // }
-  // if(distanceBetween([0,0], [10,0]) != 10){
-  //   console.log("error")
-  // }
-  // if(distanceBetween([0,0], [0,10]) != 10){
-  //   console.log("error")
-  // }
-  // if(distanceBetween([0,0], [0,-10]) != 10){
-  //   console.log("error")
-  // }
-  // if(distanceBetween([0,0], [3,4]) != 5){
-  //   console.log("error")
-  // }
 }
 
